99e521400ce691dfff8d2d2e26748ba7765d7526
ayushan294-hash <ayushan294@gmail.com>
2026-09-12 19:21:42 +0900

    Bump cache for desktop individual layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hom-league-v9-less-sticky';
+const CACHE='hom-league-v10-desktop-individual';
 const APP=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
 
 self.addEventListener('install',e=>{
