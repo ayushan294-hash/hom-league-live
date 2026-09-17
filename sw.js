62b631040801db128c26406c2acd60e4be77ff25
ayushan294-hash <ayushan294@gmail.com>
2026-09-17 22:26:54 +0900

    Refresh cache for sticky column fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hom-league-v28-update-groups';
+const CACHE='hom-league-v29-sticky-columns';
 const APP=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png','./individual-stats-v24.js'];
 
 function patchHtml(text){
